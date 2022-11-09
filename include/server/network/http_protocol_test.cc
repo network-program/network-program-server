c61c168872448301d3c142551c40ad1811ee34d2
Tony <cosgenio@gmail.com>
2022-11-10 00:34:59 +0900

    Add request parser & fix parse bugs

--- include/server/network/http_protocol_test.cc
+++ include/server/network/http_protocol_test.cc
@@ -15,6 +15,30 @@ do {                            \
   std::terminate();             \
 } while(false)
 
+#define TEST_PASS               \
+do {                            \
+  std::cerr                     \
+      << "Test passed at line " \
+      << __LINE__ << '\n';      \
+} while (false)
+
+constexpr const char* kRequest = "POST /test.jpg HTTP/1.1\r\n"
+                                 "Host: localhost:8000\r\n"
+                                 "User-Agent: Mozilla/5.0 Chrome/99.99\r\n"
+                                 "Content-Type: Text\r\n"
+                                 "Content-Length: 15\r\n"
+                                 "\r\n"
+                                 "Hello, network!";
+
+constexpr const char* kResponse = "HTTP/1.1 403 FORBIDDEN\r\n"
+                                  "Server: Apache\r\n"
+                                  "Content-Type: text/html; charset=iso-8895-1\r\n"
+                                  "Date: Sun, 6 Nov 2022 20:54:51 GMT\r\n"
+                                  "\r\n"
+                                  "<!DOCTYPE HTML PUBLIC \"-//IETF/DTD HTML 2.0//EN\">"
+                                  "<h1>FORBIDDEN</h1>";
+
+
 int main() {
 
   {
@@ -31,14 +55,39 @@ int main() {
 
     const auto p = generator.GenerateNext();
     if (!p) TEST_FAIL;
-    if (const auto view = p->string_view();
-        view != "POST /test.jpg HTTP/1.1\r\n"
-                "Host: localhost:8000\r\n"
-                "User-Agent: Mozilla/5.0 Chrome/99.99\r\n"
-                "Content-Type: Text\r\n"
-                "Content-Length: 15\r\n"
-                "\r\n"
-                "Hello, network!") TEST_FAIL;
+    if (const auto view = p->string_view(); view != kRequest) TEST_FAIL;
+  }
+
+  auto check_key_value = [](const auto& map, const auto& key, const auto& value) {
+    const auto it = map.find(key);
+    if (it == map.end()) return false;
+    if (it->second != value) return false;
+    return true;
+  };
+
+  { // Parse request
+    network::HTTPProtocol parser;
+    if (const auto b = parser.parse(kRequest); !b) TEST_FAIL; else TEST_PASS;
+    if (parser.http_method() != "POST") TEST_FAIL; else TEST_PASS;
+    if (parser.request_target() != "/test.jpg") TEST_FAIL; else TEST_PASS;
+    if (parser.http_version() != "HTTP/1.1") TEST_FAIL; else TEST_PASS;
+
+    const auto& header = parser.header();
+    if (!check_key_value(header, "Host", "localhost:8000")) TEST_FAIL; else TEST_PASS;
+    if (!check_key_value(header, "User-Agent", "Mozilla/5.0 Chrome/99.99")) TEST_FAIL; else TEST_PASS;
+    if (!check_key_value(header, "Content-Type", "Text")) TEST_FAIL; else TEST_PASS;
+    if (!check_key_value(header, "Content-Length", "15")) TEST_FAIL; else TEST_PASS;
+
+    if (const auto b = parser.parse("POST /key.txt HTTP1.1\r\n\r\n"); !b) TEST_FAIL; else TEST_PASS;
+  }
+
+  { // Parse invalid request
+    network::HTTPProtocol parser;
+
+    if (const auto b = parser.parse("You are such a moron"); b) TEST_FAIL; else TEST_PASS;
+    if (const auto b = parser.parse("POST /key.txt HTTP1.1\r\n"); b) TEST_FAIL; else TEST_PASS;
+    if (const auto b = parser.parse("POST /key.txt HTTP1.1\r\nheader:key"); b) TEST_FAIL; else TEST_PASS;
+    if (const auto b = parser.parse("POST /key.txt HTTP1.1\r\nheader:key\r\n"); b) TEST_FAIL; else TEST_PASS;
   }
 
   { // Make response
@@ -55,17 +104,8 @@ int main() {
   }
 
   {
-    constexpr const char* response =
-      "HTTP/1.1 403 FORBIDDEN\r\n"
-      "Server: Apache\r\n"
-      "Content-Type: text/html; charset=iso-8895-1\r\n"
-      "Date: Sun, 6 Nov 2022 20:54:51 GMT\r\n"
-      "\r\n"
-      "<!DOCTYPE HTML PUBLIC \"-//IETF/DTD HTML 2.0//EN\">"
-      "<h1>FORBIDDEN</h1>";
-
     network::HTTPProtocol parser;
-    parser.parse(response);
+    parser.parse(kResponse);
 
     if (parser.status_code() != 403) TEST_FAIL;
     if (parser.http_version() != "HTTP/1.1") TEST_FAIL;
